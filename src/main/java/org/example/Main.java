9cc3cb995b0075615fa4ceaad16da94d943ffd79
milicag93 <mickomali93@gmail.com>
2024-11-15 19:42:55 -0500

    second commit

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -3,9 +3,11 @@ package org.example;
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world!");
+        System.out.println("Second commit");
+        //second version
 
 
 
-        
+
     }
 }
